9403286eda18efc2ee4bfeba6bf7a2498c04d5de
Lee Lup Yuen <luppy@appkaki.com>
2018-07-08 21:54:04 +0800

    Changed to blinky sample

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,50 @@
-//! Prints "Hello, world" on the OpenOCD console
+//! Blinks an LED
 
 #![deny(unsafe_code)]
 #![deny(warnings)]
-#![no_main]
 #![no_std]
+#![no_main]
 
+extern crate cortex_m;
 #[macro_use]
 extern crate cortex_m_rt as rt;
-extern crate cortex_m_semihosting as sh;
 extern crate panic_semihosting;
-extern crate stm32f103xx_hal;
+extern crate stm32f103xx_hal as hal;
+#[macro_use(block)]
+extern crate nb;
 
-use core::fmt::Write;
-
+use hal::prelude::*;
+use hal::stm32f103xx;
+use hal::timer::Timer;
 use rt::ExceptionFrame;
-use sh::hio;
 
 entry!(main);
 
 fn main() -> ! {
-    let mut hstdout = hio::hstdout().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f103xx::Peripherals::take().unwrap();
 
-    writeln!(hstdout, "Hello, world!").unwrap();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
 
-    loop {}
+    // Try a different clock configuration
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    // let clocks = rcc.cfgr
+    //     .sysclk(64.mhz())
+    //     .pclk1(32.mhz())
+    //     .freeze(&mut flash.acr);
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    // Try a different timer (even SYST)
+    let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
+    loop {
+        block!(timer.wait()).unwrap();
+        led.set_high();
+        block!(timer.wait()).unwrap();
+        led.set_low();
+    }
 }
 
 exception!(HardFault, hard_fault);
