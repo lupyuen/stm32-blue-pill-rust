7940124eeb4f606f125a102a99050e018a5c6999
lupyuen <luppy@appkaki.com>
2018-07-21 11:53:04 +0800

    Fixing LED

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -8,7 +8,7 @@ extern crate cortex_m;
 extern crate cortex_m_rtfm as rtfm;
 extern crate stm32f103xx_hal as hal;  //  Hardware Abstraction Layer (HAL) for STM32 Blue Pill.
 
-use blue_pill::led::{self, Green};
+use blue_pill::led::{self, LD13};
 use cortex_m::peripheral::SystClkSource;
 use rtfm::{app, Threshold};
 
@@ -20,96 +20,28 @@ mod blue_pill {
 
         use hal::prelude::*;
 
-        use hal::gpio::gpioe::{self, PE10, PE11, PE12, PE13, PE14, PE15, PE8, PE9, PEx};
+        //// use hal::gpio::gpioe::{self, PE10, PE11, PE12, PE13, PE14, PE15, PE8, PE9, PEx};
+        use hal::gpio::gpioc::{self, PC13};
         use hal::gpio::{Output, PushPull};
 
-        ///  North LED
-        pub type LD3 = PE9<Output<PushPull>>;
-
-        /// Northeast LED
-        pub type LD5 = PE10<Output<PushPull>>;
-
-        /// East LED
-        pub type LD7 = PE11<Output<PushPull>>;
-
-        /// Southeast LED
-        pub type LD9 = PE12<Output<PushPull>>;
-
-        /// South LED
-        pub type LD10 = PE13<Output<PushPull>>;
-
-        /// Southwest LED
-        pub type LD8 = PE14<Output<PushPull>>;
-
-        /// West LED
-        pub type LD6 = PE15<Output<PushPull>>;
-
-        /// Northwest LED
-        pub type LD4 = PE8<Output<PushPull>>;
-
-        /// Cardinal directions. Each one matches one of the user LEDs.
-        pub enum Direction {
-            /// North / LD3
-            North,
-            /// Northeast / LD5
-            Northeast,
-            /// East / LD7
-            East,
-            /// Southeast / LD9
-            Southeast,
-            /// South / LD10
-            South,
-            /// Southwest / LD8
-            Southwest,
-            /// West / LD6
-            West,
-            /// Northwest / LD4
-            Northwest,
-        }
+        //// pub type LD10 = PE13<Output<PushPull>>;
+        pub type LD13 = PC13<Output<PushPull>>;
 
         /// Array of all the user LEDs on the board
         pub struct Leds {
-            leds: [Led; 8],
+            leds: [Led; 1],
         }
 
         impl Leds {
             /// Initializes all the user LEDs
-            pub fn new(mut gpioe: gpioe::Parts) -> Self {
-                let n = gpioe
-                    .pe9
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let ne = gpioe
-                    .pe10
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let e = gpioe
-                    .pe11
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let se = gpioe
-                    .pe12
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let s = gpioe
-                    .pe13
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let sw = gpioe
-                    .pe14
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let w = gpioe
-                    .pe15
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
-                let nw = gpioe
-                    .pe8
-                    .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper);
+            pub fn new(mut gpioc: gpioc::Parts) -> Self {
+                let ld13 = gpioc
+                    .pc13
+                    .into_push_pull_output(&mut gpioc.moder, &mut gpioc.otyper);
 
                 Leds {
                     leds: [
-                        n.into(),
-                        ne.into(),
-                        e.into(),
-                        se.into(),
-                        s.into(),
-                        sw.into(),
-                        w.into(),
-                        nw.into(),
+                        ld13.into(),
                     ],
                 }
             }
@@ -176,7 +108,7 @@ mod blue_pill {
             }
         }
 
-        ctor!(LD3, LD4, LD5, LD6, LD7, LD8, LD9, LD10);
+        ctor!(LD13);
 
         impl Led {
             /// Turns the LED off
@@ -234,8 +166,8 @@ fn toggle(_t: &mut Threshold, r: SYS_TICK::Resources) {
     **r.ON = !**r.ON;
 
     if **r.ON {
-        Green.on();
+        LD13.on();
     } else {
-        Green.off();
+        LD13.off();
     }
 }
